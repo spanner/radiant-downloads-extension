d287994e08b34ee8e2ba299335a9d2dc4583e799
will <will@spanner.org>
2009-10-09 14:43:52 +0100

    redirect to url, not path, and make url relative

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -5,9 +5,9 @@ class DownloadsController < ReaderActionController
   def show
     @download = Download.find(params[:id])
     if @download.available_to?(current_reader)
-      response.headers['X-Accel-Redirect'] = @download.document.path
+      response.headers['X-Accel-Redirect'] = @download.document.url
       response.headers["Content-Type"] = @download.document_content_type
-      response.headers['Content-Disposition'] = "attachment; filename=\"#{@download.document_file_name}\"" 
+      response.headers['Content-Disposition'] = "attachment; filename=#{@download.document_file_name}" 
       response.headers['Content-Length'] = @download.document_file_size
       render :nothing => true
     else
